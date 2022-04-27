767b0576b76bb659a366cb4f8372d1745873fcbf
HuskyCreek <juands1903@gmail.com>
2022-04-26 23:55:30 -0500

    Create Api Nextjs

---
+++ .next/server/pages/api/avo.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/avo";
+exports.ids = ["pages/api/avo"];
+exports.modules = {
+
+/***/ "(api)/./pages/api/avo/index.ts":
+/*!********************************!*\
+  !*** ./pages/api/avo/index.ts ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst AllCats = async (request, response)=>{\n    response.end(JSON.stringify({\n        hello: \"Mery\"\n    }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AllCats);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9wYWdlcy9hcGkvYXZvL2luZGV4LnRzLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFFQSxNQUFNQSxPQUFPLEdBQUcsT0FBT0MsT0FBd0IsRUFBRUMsUUFBd0IsR0FBSztJQUM1RUEsUUFBUSxDQUFDQyxHQUFHLENBQUNDLElBQUksQ0FBQ0MsU0FBUyxDQUFDO1FBQUVDLEtBQUssRUFBRSxNQUFNO0tBQUUsQ0FBQyxDQUFDLENBQUM7Q0FDakQ7QUFFRCxpRUFBZU4sT0FBTyxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbmV4dGpzcHJvamVjdC0xLy4vcGFnZXMvYXBpL2F2by9pbmRleC50cz9iY2Q1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IEluY29taW5nTWVzc2FnZSwgU2VydmVyUmVzcG9uc2UgfSBmcm9tIFwiaHR0cFwiO1xyXG5cclxuY29uc3QgQWxsQ2F0cyA9IGFzeW5jIChyZXF1ZXN0OiBJbmNvbWluZ01lc3NhZ2UsIHJlc3BvbnNlOiBTZXJ2ZXJSZXNwb25zZSkgPT4ge1xyXG4gIHJlc3BvbnNlLmVuZChKU09OLnN0cmluZ2lmeSh7IGhlbGxvOiAnTWVyeScgfSkpO1xyXG59O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgQWxsQ2F0cztcclxuIl0sIm5hbWVzIjpbIkFsbENhdHMiLCJyZXF1ZXN0IiwicmVzcG9uc2UiLCJlbmQiLCJKU09OIiwic3RyaW5naWZ5IiwiaGVsbG8iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(api)/./pages/api/avo/index.ts\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./pages/api/avo/index.ts"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ .next/server/pages/api/cats.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/cats";
+exports.ids = ["pages/api/cats"];
+exports.modules = {
+
+/***/ "(api)/./pages/api/cats/index.ts":
+/*!*********************************!*\
+  !*** ./pages/api/cats/index.ts ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst AllCats = async (request, response)=>{\n    response.end(JSON.stringify({\n        hello: \"World\"\n    }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AllCats);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9wYWdlcy9hcGkvY2F0cy9pbmRleC50cy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBRUEsTUFBTUEsT0FBTyxHQUFHLE9BQU9DLE9BQXdCLEVBQUVDLFFBQXdCLEdBQUs7SUFDNUVBLFFBQVEsQ0FBQ0MsR0FBRyxDQUFDQyxJQUFJLENBQUNDLFNBQVMsQ0FBQztRQUFFQyxLQUFLLEVBQUUsT0FBTztLQUFFLENBQUMsQ0FBQyxDQUFDO0NBQ2xEO0FBRUQsaUVBQWVOLE9BQU8sRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL25leHRqc3Byb2plY3QtMS8uL3BhZ2VzL2FwaS9jYXRzL2luZGV4LnRzPzdhMzYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgSW5jb21pbmdNZXNzYWdlLCBTZXJ2ZXJSZXNwb25zZSB9IGZyb20gXCJodHRwXCI7XHJcblxyXG5jb25zdCBBbGxDYXRzID0gYXN5bmMgKHJlcXVlc3Q6IEluY29taW5nTWVzc2FnZSwgcmVzcG9uc2U6IFNlcnZlclJlc3BvbnNlKSA9PiB7XHJcbiAgcmVzcG9uc2UuZW5kKEpTT04uc3RyaW5naWZ5KHsgaGVsbG86ICdXb3JsZCcgfSkpO1xyXG59O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgQWxsQ2F0cztcclxuIl0sIm5hbWVzIjpbIkFsbENhdHMiLCJyZXF1ZXN0IiwicmVzcG9uc2UiLCJlbmQiLCJKU09OIiwic3RyaW5naWZ5IiwiaGVsbG8iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(api)/./pages/api/cats/index.ts\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./pages/api/cats/index.ts"));
+module.exports = __webpack_exports__;
+
+})();
