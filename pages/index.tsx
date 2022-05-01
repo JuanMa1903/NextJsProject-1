430136da321a1a5d6194617cb852aac01b719c51
HuskyCreek <juands1903@gmail.com>
2022-04-30 23:43:55 -0500

    It´s app dude no aoo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import ProductList from '@components/ProductList/ProductList'
 import fetch from "isomorphic-unfetch";
 
 export const getStaticProps = async () => { 
-    const response = await fetch('https://platzi-avo.vercel.aoo/api/avo')
+    const response = await fetch('https://platzi-avo.vercel.app/api/avo')
     const {data: productList}: TAPIAvoResponse = await response.json()
       }
   return{
